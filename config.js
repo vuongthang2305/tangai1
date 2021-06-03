bf34d0b49235aa3d2bcba973f3266c7f2912456d
vuongthang <thangpro767@gmail.com>
2021-06-03 13:16:21 +0700

    edit

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ const CONFIG = {
     Đây là lựa chọn cuối rồi`,
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnReply: 'Gửi cho mình <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Chốt thế nha :3',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://m.me/theng.theng.2305' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/theng.theng.2305'
 }

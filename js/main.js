9d668e7e586ee50e3e766b0e192f0f7361b987bd
vuongthang <thangpro767@gmail.com>
2021-06-03 22:31:05 +0700

    edit

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function firstQuestion(){
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        imageUrl: 'img/mymy.jpg',
+        imageUrl: 'img/lookMe.jpg',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("img/iput-bg.jpg")',
